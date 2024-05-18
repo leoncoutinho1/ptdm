a56307c47d5cf075b9ab9509e063e717619f3a8d
leoncoutinho1 <leon.coutinho1@gmail.com>
2024-05-18 09:44:10 -0300

    Change barcode reader

--- padaria_frontend/src/components/BarcodeReader.vue
+++ padaria_frontend/src/components/BarcodeReader.vue
@@ -1,10 +1,13 @@
 <template>
-    <div id="interactive" class="viewport"></div>
-    <Button label="Close" icon="pi pi-times" @click="stopScan" />
+    <div id="interactive" class="viewport">
+        <qrcode-stream :formats="['ean_13']" @detect="searchCode"></qrcode-stream>
+        </div>
+    <Button label="Close" icon="pi pi-times" @click="stopScan" ></Button>
 </template>
 
 <script>
-    import Quagga from 'quagga';
+    import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+    // import Quagga from 'quagga';
     import Button from 'primevue/button';
     import Dialog from 'primevue/dialog';
     import HTTP from '../helpers/http-common';
@@ -12,31 +15,7 @@
     export default {
         name: "BarcodeReader",
         data() {
-            return {
-                code: "",
-                quaggaConfig: {
-                    inputStream: {
-                        type : "LiveStream",
-                        constraints: {
-                            width: {max: 320},
-                            height: {max: 240},
-                            aspectRatio: {min: 1, max: 100},
-                            facingMode: "environment" // or "user" for the front camera
-                        }
-                    },
-                    locator: {
-                        patchSize: "medium",
-                        halfSample: true
-                    },
-                    numOfWorkers: (navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
-                    decoder: {
-                        "readers":[
-                            {"format":"ean_reader","config":{}}
-                        ]
-                    },
-                    locate: true
-                }              
-            }
+            return {}
         },
         props: ['showModal'],
         emits: [
@@ -45,47 +24,30 @@
             'editProduct'
         ],
         methods: {
-            async searchCode(code) {
-                await HTTP.get(`/Product/GetProductByBarcode?barcode=${code}`)
+            async searchCode(codes) {
+                console.log(codes);
+                await HTTP.get(`/Product/GetProductByBarcode?barcode=${codes[0].rawValue}`)
                     .then(response => {
                         var product = response.data;
                         if (product != undefined) {
-                            Quagga.stop();
-                            alert(product.ToString());
                             this.$emit('editProduct', product);
+                            this.$emit('closeModal');
                         }
                     });
-            },
-            startScan() {
-                this.displayModal = true;
-                setTimeout(() => {
-                    Quagga.init(this.quaggaConfig, 
-                        function(err) {
-                            if (err) {
-                                console.log(err);
-                                this.stopScan();
-                                return;
-                            }
-                            Quagga.start();
-                        }
-                    );
-                    Quagga.onDetected((res) => {
-                        this.searchCode(res.codeResult.code);
-                        this.stopScan();
-                    });
-                }, 1000);
             },
             stopScan() {
-                Quagga.stop();
                 this.$emit('closeModal');
             }            
         },
         created() {
-            this.startScan();
+            // this.startScan();
         },
         components: {
             Button,
-            Dialog
+            Dialog,
+            QrcodeStream,
+            QrcodeDropZone,
+            QrcodeCapture
         }
     }
 </script>
