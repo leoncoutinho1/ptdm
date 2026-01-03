8ed02005cd807604860d872992c9bad173972650
leoncoutinho1 <leon.coutinho1@hotmail.com>
2026-01-03 18:06:13 -0300

    Converte para PWA

---
+++ ptdm_react/vite.config.ts
@@ -0,0 +1,60 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Controle de Estoque',
+        short_name: 'PDV',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#1e40af',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    })
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5215',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq, req) => {
+            const url = req.url || '';
+            console.log('Proxying:', url, '->', 'http://localhost:5215' + url.replace(/^\/api/, ''))
+          })
+        }
+      },
+    },
+  },
+
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: './vitest.setup.mjs',
+  // },
+});
